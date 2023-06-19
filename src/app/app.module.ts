4eff47acb4066e8cc2265ba8b250ec1a59d3d14b
sreelakshmi v kammath <sreelakshmi.kammath@experionglobal.com>
2023-06-19 07:41:49 +0530

    Removed Employee Component. Need to Add Employee Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
